7455e5a6279a804a53fcaa03abf23ed3568c5bd9
kenneth <hkenneth939@gmail.com>
2021-11-07 13:15:38 -0500

    Proyecto montado con template hyperspace, usando Spring. bbdd y thymeleaddqq git statu}++ +s

---
+++ src/main/java/com/herrera/kenneth/app/MiPortafolioApplication.java
@@ -0,0 +1,13 @@
+package com.herrera.kenneth.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPortafolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPortafolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herrera/kenneth/app/MiPortafolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herrera.kenneth.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPortafolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
